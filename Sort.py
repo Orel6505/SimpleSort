73a5b6b890b202bde790b32567b9180132ea3e68
Orel Yosupov <Orely4242@gmail.com>
2024-02-09 00:40:39 +0200

    SimpleSort: Few Changes

--- Sort.py
+++ Sort.py
@@ -1,12 +1,11 @@
-import os, argparse
+import os, argparse, json
 from pathlib import Path
 
 def main():
     ## Arguments
-    SArg = Arguments()
+    Arg = Arguments()
     
-    location = SArg["location"]
-    guess = SArg["guess"]
+    location = Arg["location"]
     
     # Absolute Path = Full Path
     # Relative Path = Part of a Path
@@ -14,28 +13,40 @@ def main():
     
     #All of this is dangerous and I don't want crash, so let's use try
     try:
-        location = CheckLocation(location)
+        location = Path(IsLocation(location))
     except Exception as e:
-        print(e.args)
-    finally:
-        print("Done")
-        
+        print("The Error is: ", e.args)
+    GetAllFiles(location)
+
 #Pass All Arguments
 def Arguments():
     parser = argparse.ArgumentParser(description="Just a simple sorting script",formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument("-l", "--location", type=str, nargs='?', default=Path.home()/"Downloads", help="Custom location to sort, default is Downloads folder")
-    parser.add_argument("-g", "--guess", type=bool, nargs='?', default=False, help="Guess The location")
     args = vars(parser.parse_args())
-    print(args)
     return args
 
 #Is the location real?
-def CheckLocation(location):
+def IsLocation(location):
     if (not Path.is_absolute(location)):
         os.path.abspath(location)
         if(not Path.exists(location)):
             return None
     return location
-        
+
+def IsFile(filepath):
+    if not(os.path.getsize(filepath) or os.path.getsize(filepath)==0): return False
+    if (Path(filepath).suffix == '' and Path.is_dir(filepath)): return False
+    return True
+
+#Creates Object File for all files in this directory and sub directories
+def GetAllFiles(location):
+    for file in os.listdir(location):
+        filepath = location/file
+        if (IsFile(filepath)):
+            print("#move files to desired location")
+        else:
+            print("#move folder to desired location")
+
+#Define as script
 if __name__ == "__main__":
-    main() #define as script
+    main()
